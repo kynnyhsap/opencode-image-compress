050214c6c0d8a4710bbcdb039585a6a9d5da0a3c
Andriy Pashynnyk <tobirawork@gmail.com>
2026-01-30 15:45:37 +0200

    tweak

--- dev.ts
+++ dev.ts
@@ -14,12 +14,16 @@ console.log('')
 const pluginPath = pathToFileURL(scriptDir).href
 console.log(`📦 Plugin path: ${pluginPath}`)
 
+const config = { plugin: [pluginPath] } satisfies Config
+
+const OPENCODE_CONFIG_CONTENT = JSON.stringify(config)
+
+// console.log(`OPENCODE_CONFIG_CONTENT='${OPENCODE_CONFIG_CONTENT}' opencode`)
+
 const proc = spawn(['opencode'], {
 	env: {
 		...process.env,
-		OPENCODE_CONFIG_CONTENT: JSON.stringify({
-			plugin: [pluginPath],
-		} satisfies Config),
+		OPENCODE_CONFIG_CONTENT,
 	},
 	stdin: 'inherit',
 	stdout: 'inherit',
